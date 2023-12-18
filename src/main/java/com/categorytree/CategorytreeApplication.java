bfc0772ac89fa2f7b1ea28357f4984e7b6d68609
Jaswine <vaselesstolarov813@gmail.com>
2023-12-18 18:53:09 +0600

    added basic structure

---
+++ src/main/java/com/categorytree/CategorytreeApplication.java
@@ -0,0 +1,13 @@
+package com.categorytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorytreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorytreeApplication.class, args);
+	}
+
+}
